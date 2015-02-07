3be83b28385c10b0573ecd5aca7f8524e6b8583c
Grace Yao <ygrace6526@gmail.com>
2015-02-07 01:47:25 -0800

    fix winning

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,6 @@
 var board = [0,0,0,0,0,0,0,0,0];
 var turn = 1;
-var win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8]];
+var win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 var spaces = ["rowOneColOne", "rowOneColTwo", "rowOneColThree", 
                     "rowTwoColOne", "rowTwoColTwo", "rowTwoColThree",
                     "rowThreeColOne", "rowThreeColTwo", "rowThreeColThree"];
